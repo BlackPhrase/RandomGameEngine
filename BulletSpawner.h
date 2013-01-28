db8ec3f946e5d43221937fa57913d3f6a6db7630
Jordan Cristiano <jfcristi@uwaterloo.ca>
2013-01-27 23:17:56 -0500

    forgot this

---
+++ BulletSpawner.h
@@ -0,0 +1,13 @@
+
+#ifndef BULLET_SPAWNER_H
+#define BULLET_SPAWNER_H
+
+class CBullet;
+
+class IBulletSpawner
+{
+public:
+	virtual void SpawnBullet( CBullet *pBullet ) = 0;
+};
+
+#endif // BULLET_SPAWNER_H
